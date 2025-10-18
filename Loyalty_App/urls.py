d9c46cb1f04cbe13c9d3261e73bfe41aa44b0bb3
clientwaqas724-ctrl <clientwaqas724@gmail.com>
2025-10-18 18:28:41 +0500

    Add CheckCouponRedemptionView to URL patterns

--- Loyalty_App/urls.py
+++ Loyalty_App/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TransactionViewSet
-
+from .views import CheckCouponRedemptionView   # ✅ import your view
+##################################################################################################
 router = DefaultRouter()
 router.register(r'transactions', TransactionViewSet, basename='transaction')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("check-coupon-redemption/",CheckCouponRedemptionView.as_view(),name="check-coupon-redemption")
 ]
