022aeb0771103b51d5713216aa8eb304b96613c7
clientwaqas724-ctrl <clientwaqas724@gmail.com>
2025-11-03 16:12:06 +0500

    Update urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from User_App import views
 urlpatterns = [
+    path('', views.My_Home, name='home'),
     path('admin/', admin.site.urls),
     path('api/user/', include('User_App.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
@@ -15,4 +17,5 @@ urlpatterns = [
 ########################################################################################################################
 #Serve media and (optionally) static files during development only
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
